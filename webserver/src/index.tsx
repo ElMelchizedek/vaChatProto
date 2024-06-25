19fe8d0284352eeb2864a2e39404f33fbe5cbca5
claire <102092516+dragonflyfree@users.noreply.github.com>
2024-06-25 23:55:07 +1000

    simple websockets

--- webserver/src/index.tsx
+++ webserver/src/index.tsx
@@ -1,16 +1,65 @@
 import { Elysia } from 'elysia'
 import { html } from '@elysiajs/html'
+import { swagger } from '@elysiajs/swagger'
+
+const messages: string[] = []
+const send_queues: string[][] = []
+let user_id = 0
 
 new Elysia()
     .use(html())
-    .get('/jsx', () => (
+    .use(swagger())
+    .get('/', () =>
         <html lang='en'>
             <head>
-                <title>Hello World</title>
+                <title>Message others</title>
+
+                <script src="https://unpkg.com/htmx.org@2.0.0"></script>
+                <script src="https://unpkg.com/htmx-ext-ws@2.0.0/ws.js"></script>
             </head>
             <body>
                 <h1>Hello World</h1>
+
+                <div hx-ext="ws" ws-connect="/ws">
+                    <div id="messages"></div>
+
+                    <form id="write-message" ws-send>
+                        <input name="message" />
+                    </form>
+                </div>
             </body>
         </html>
-    ))
+    )
+    .ws('/ws', {
+        async open(ws) {
+            const ws_id = user_id++
+            send_queues[ws_id] = [...messages]
+
+            setInterval(
+                async () => {
+                    ws.send(
+                        <div id="messages" hx-swap-oob="beforeend">
+                            {
+                                send_queues[ws_id].map(message => 
+                                    <p safe>
+                                        {message}
+                                    </p>
+                                )
+                            }
+                        </div>
+                    )
+
+                    send_queues[ws_id] = []
+                }, 
+                200
+            )
+        },
+
+        message(ws, message) {
+            const msg = (message as any).message
+
+            messages.push(msg)
+            send_queues.forEach(queue => queue.push(msg))
+        }
+    })
     .listen(3000)
